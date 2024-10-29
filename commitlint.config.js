8f23cecc7c19ed55ed258cfba9cd1d1253c820eb
Francois Dion <francoisdion71@gmail.com>
2024-10-29 15:15:50 +0100

    chore(fast-housing-front): added commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,14 @@ module.exports = {
     extends: [
         "@commitlint/config-conventional"
     ],
+    rules: {
+        "type-enum": [2, "always", [
+            "feat", "fix", "docs", "style", "refactor", "perf", "test", "chore", "revert"
+        ]],
+        "scope-enum": [2, "always", ["fast-housing-front", "email-service", "face-detection-service"]],
+        "subject-full-stop": [0, "never"],
+        "subject-case": [0, "never"],
+        "header-max-length": [2, "always", 80],
+        "body-max-line-length": [2, "always", 72],
+    }
 }
